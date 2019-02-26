d544a6755b7e4ac05c614e4c1474be507c93fcfe
Taulant Vokshi <taulantvokshi2@gmail.com>
2019-02-26 16:23:50 -0500

    get products routs done

--- server/api/products.js
+++ server/api/products.js
@@ -1,2 +1,26 @@
 const router = require('express').Router()
+const {Products} = require('../db/models')
 module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Products.findAll()
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  const id = req.params.id
+  try {
+    const product = await Products.findById(Number(id))
+    if (product) {
+      res.json(product)
+    } else {
+      console.error('No product found!!!') // check this latter!
+    }
+  } catch (err) {
+    next(err)
+  }
+})
