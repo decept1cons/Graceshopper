f9c952024c1a652d440505f6e0aa762d734645c0
Terence Thai <TerenceThai@gmail.com>
2019-02-25 20:18:11 -0500

    test

---
+++ server/db/models/products.spec.js
@@ -0,0 +1,28 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const db = require('../index')
+const Products = db.model('products')
+
+describe('Products model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('instanceMethods', () => {
+    describe('correctPassword', () => {
+      let sox
+
+      beforeEach(async () => {
+        sox = await Products.create({
+          name: 'sox',
+          type: 'clothing'
+        })
+      })
+
+      it('returns true if the password is correct', () => {
+        expect(sox.name('sox')).to.be.equal(true)
+      })
+    }) // end describe('correctPassword')
+  }) // end describe('instanceMethods')
+}) // end describe('User model')
