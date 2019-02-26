43c3960cb3074c8e2ebf3086ae16ba355f655f83
Nick <nickpereslugoff@gmail.com>
2019-02-26 12:39:12 -0500

    Added styling to ProductsGrid

--- client/components/ProductsGrid.js
+++ client/components/ProductsGrid.js
@@ -1,9 +1,40 @@
 import React, {Component} from 'react'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import {withStyles} from '@material-ui/core/styles'
+
+const styles = () => ({
+  paper: {
+    height: 140,
+    width: 100
+  }
+})
 
 class ProductsGrid extends Component {
   render() {
-    return <div>Mamma mia</div>
+    const {classes} = this.props
+
+    return (
+      <Grid container spacing={16} justify="center">
+        <Grid item lg={3}>
+          <Paper className={classes.paper}>
+            <Typography component="h3">This is a product</Typography>
+          </Paper>
+        </Grid>
+        <Grid item lg={3}>
+          <Paper className={classes.paper}>
+            <Typography component="h3">This is a product</Typography>
+          </Paper>
+        </Grid>
+        <Grid item lg={3}>
+          <Paper className={classes.paper}>
+            <Typography component="h3">This is a product</Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+    )
   }
 }
 
-export default ProductsGrid
+export default withStyles(styles)(ProductsGrid)
