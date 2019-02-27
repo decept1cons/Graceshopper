a55c0d9a435635ffccc1f7ef4be322ec9391f266
Derek Harmanli <derek.harmanli@gmail.com>
2019-02-27 16:56:50 -0500

    fix(model) - pluralization error of products model

--- server/api/products.js
+++ server/api/products.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
-const {Products, User} = require('../db/models')
+const {Product, User} = require('../db/models')
 const db = require('../db/db')
 
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const products = await Products.findAll()
+    const products = await Product.findAll()
 
     res.json(products)
   } catch (err) {
@@ -17,7 +17,7 @@ router.get('/', async (req, res, next) => {
 router.get('/:id', async (req, res, next) => {
   const id = req.params.id
   try {
-    const product = await Products.findById(Number(id))
+    const product = await Product.findById(Number(id))
     if (product) {
       res.json(product)
     } else {
