0d4e8da3afedf5904317459d4d4fe4f0f2f6790b
Taulant Vokshi <taulantvokshi2@gmail.com>
2019-02-26 19:53:42 -0500

    put route for card in progress

--- server/api/products.js
+++ server/api/products.js
@@ -1,15 +1,8 @@
 const router = require('express').Router()
-const {Products} = require('../db/models')
-module.exports = router
+const {Products, User} = require('../db/models')
+const db = require('../db/db')
 
-router.get('/', async (req, res, next) => {
-  try {
-    const products = await Products.findAll()
-    res.json(products)
-  } catch (err) {
-    next(err)
-  }
-})
+module.exports = router
 
 router.get('/:id', async (req, res, next) => {
   const id = req.params.id
@@ -20,6 +13,28 @@ router.get('/:id', async (req, res, next) => {
     } else {
       console.error('No product found!!!') // check this latter!
     }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/cart/:productId', async (req, res, next) => {
+  const productId = req.params.productId
+  // const userId = req.session.user.id
+  try {
+    const user = await User.findById(Number(4))
+    await user.addProducts(Number(productId))
+    const products = await db.models.cart.findAll()
+    res.send(products)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Products.findAll()
+    res.json(products)
   } catch (err) {
     next(err)
   }
