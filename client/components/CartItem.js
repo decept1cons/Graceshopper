b741ba5aeb8ca8fe33efd1b81d6d6252cbaed0c4
Terence Thai <TerenceThai@gmail.com>
2019-03-02 16:17:15 -0500

    fixed remove product function

--- client/components/CartItem.js
+++ client/components/CartItem.js
@@ -11,40 +11,42 @@ const mapDispatchToProps = dispatch => ({
   getCart: id => dispatch(fetchCart(id))
 })
 
-export default withRouter(props => {
-  if (!props.cart.userId) {
-    return (
-      <Table.Row>
-        <Table.Cell>
-          <img className="cartItemImage" src={props.cart.imageUrl} />
-        </Table.Cell>
-        <Table.Cell>{props.cart.name}</Table.Cell>
-        <Table.Cell>{`$${props.cart.price}`}</Table.Cell>
-        <Table.Cell>{props.cart.quantity}</Table.Cell>
-        <Table.Cell>
-          <Button content="Remove" />
-        </Table.Cell>
-      </Table.Row>
-    )
-  } else {
-    return (
-      <Table.Row>
-        <Table.Cell>
-          <img className="cartItemImage" src={props.cart.product.imageUrl} />
-        </Table.Cell>
-        <Table.Cell>{props.cart.product.name}</Table.Cell>
-        <Table.Cell>{`$${props.cart.price}`}</Table.Cell>
-        <Table.Cell>{props.cart.quantity}</Table.Cell>
-        <Table.Cell>
-          <Button
-            content="Remove"
-            onClick={() => {
-              removeProduct(id, userId)
-              //getCart(userId)
-            }}
-          />
-        </Table.Cell>
-      </Table.Row>
-    )
-  }
-})
+export default withRouter(
+  connect(null, mapDispatchToProps)(props => {
+    if (!props.cart.userId) {
+      return (
+        <Table.Row>
+          <Table.Cell>
+            <img className="cartItemImage" src={props.cart.imageUrl} />
+          </Table.Cell>
+          <Table.Cell>{props.cart.name}</Table.Cell>
+          <Table.Cell>{`$${props.cart.price}`}</Table.Cell>
+          <Table.Cell>{props.cart.quantity}</Table.Cell>
+          <Table.Cell>
+            <Button content="Remove" />
+          </Table.Cell>
+        </Table.Row>
+      )
+    } else {
+      return (
+        <Table.Row>
+          <Table.Cell>
+            <img className="cartItemImage" src={props.cart.product.imageUrl} />
+          </Table.Cell>
+          <Table.Cell>{props.cart.product.name}</Table.Cell>
+          <Table.Cell>{`$${props.cart.price}`}</Table.Cell>
+          <Table.Cell>{props.cart.quantity}</Table.Cell>
+          <Table.Cell>
+            <Button
+              content="Remove"
+              onClick={() => {
+                props.removeProduct(props.cart.id, props.cart.userId)
+                //getCart(userId)
+              }}
+            />
+          </Table.Cell>
+        </Table.Row>
+      )
+    }
+  })
+)
